6562743f98a432a3662fa340593e366d1a0e99b9
Patrick Nicolosi <patricknicolosi99@gmail.com>
2022-04-05 19:57:33 +0200

    Update unione.dart

--- unione.dart
+++ unione.dart
@@ -1,9 +1,13 @@
-//Chiusura di una famiglia di insiemi rispetto l'operazione di unione
 void main() {
   List<List<int>> famiglia = [
-    [1],
-    [2],
-    [3],
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8],
+    [9, 10],
+    [1, 4, 5],
+    [2, 6, 8],
+    [3, 5],
+    [1, 10],
   ];
 
   List<List<int>> chiusura = [];
@@ -17,10 +21,7 @@ void main() {
       chiusura.add(unione(famiglia[i], famiglia[j]));
     }
   }
-
   chiusura.add(unioneTotale);
-  eliminaDuplicati(chiusura);
-
   print(chiusura);
 }
 
@@ -31,12 +32,3 @@ List<int> unione(List<int> a, List<int> b) {
   return c.toSet().toList();
 }
 
-void eliminaDuplicati(List<List<int>> array) {
-  for (int i = 0; i < array.length; i++) {
-    for (int j = i + 1; j < array.length; j++) {
-      if (array[i].toString() == array[j].toString()) {
-        array.removeAt(i);
-      }
-    }
-  }
-}
