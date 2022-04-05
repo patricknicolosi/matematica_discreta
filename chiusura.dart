241922fd4df34075afedea4ae4d3dcc5b4759a24
Patrick Nicolosi <patricknicolosi99@gmail.com>
2022-04-05 18:45:44 +0200

    Bug fix della chiusura di insiemi formati solo da singoletti

--- chiusura.dart
+++ chiusura.dart
@@ -1,20 +1,30 @@
 void main() {
   List<List<int>> famiglia = [
-    [1, 2, 3],
-    [1, 2],
-    [1, 3],
-    [2, 4],
+    [1],
+    [2],
+    [3],
   ];
 
   List<List<int>> chiusura = [];
-  chiusura.addAll(famiglia); //La chiusura deve contenere la famiglia
+  chiusura.addAll(
+      famiglia); //La chiusura deve contenere la famiglia per definizione
+
+  List<int> unioneTotale =
+      []; //Nella chiusura deve esserci anche l'unione di tutti gli elementi appartenenti alla famiglia
 
   //Aggiungo elementi alla famiglia fino a quando non si "stabilizza" formando cosi la chiusura rispetto l'operazione unione
   for (int i = 0; i < famiglia.length; i++) {
     for (int j = i + 1; j < famiglia.length; j++) {
+      unioneTotale = unione(
+          unioneTotale,
+          unione(famiglia[i],
+              famiglia[j])); //Effettuo l'unione di tutti gli elementi
       chiusura.add(unione(famiglia[i], famiglia[j]));
     }
   }
+
+  chiusura.add(
+      unioneTotale); //Aggiungo l'unione di tutti gli elementi alla chiusura
 
   //La chiusura è per definizione la più piccola famiglia quindi ripulisco la mia chiusura togliendone i duplicati
   eliminaDuplicati(chiusura);
