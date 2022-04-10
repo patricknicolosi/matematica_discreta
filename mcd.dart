975a668e60518b3d8dd2c3b10577b579ee17696b
Patrick Nicolosi <patricknicolosi99@gmail.com>
2022-04-10 17:25:29 +0200

    Update and rename MCD.dart to mcd.dart

---
+++ mcd.dart
@@ -0,0 +1,22 @@
+int mcdR(int a, int b) {
+  if (b == 0) return a;
+  int r = a % b;
+  return mcdR(b, r);
+}
+
+int mcdI(int a, int b) {
+  int r = a % b;
+  while (r != 0) {
+    r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
+}
+
+void main() {
+  int a = 90;
+  int b = 25;
+  print(mcdR(a, b));
+  print(mcdI(a, b));
+}
