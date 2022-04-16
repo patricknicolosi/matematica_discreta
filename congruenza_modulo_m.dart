2c862fc7fe4af5ea2c60ee26f7cfa9e9f1993549
Patrick Nicolosi <patricknicolosi99@gmail.com>
2022-04-16 11:09:36 +0200

    Create congruenza_modulo_m.dart

---
+++ congruenza_modulo_m.dart
@@ -0,0 +1,11 @@
+//Relazione binaria di congruenza modulo m
+void main() {
+  int a = 10;
+  int b = 2;
+  int m = 4;
+  if ((a % m) == (b % m)) {
+    print('I due numeri sono in relazione binaria di congruenza modulo m');
+  } else {
+    print('I due numeri non sono in relazione binaria di congruenza modulo m');
+  }
+}
