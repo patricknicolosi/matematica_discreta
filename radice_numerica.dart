0bf46c48f2b00a21299bfd98cf2b17cba8fa76af
Patrick Nicolosi <patricknicolosi99@gmail.com>
2022-04-27 16:01:47 +0200

    Create radice_numerica.dart

---
+++ radice_numerica.dart
@@ -0,0 +1,24 @@
+//Radice numerica
+
+void main() {
+  int n = 456;
+  print('Procedura ricorsiva: ' +
+      radiceNumericaRicorsiva(n.toString()).toString());
+  print('Procedura iterativa: ' +
+      radiceNumericaIterativa(n.toString()).toString());
+}
+
+int radiceNumericaIterativa(String n) {
+  int rho = 0;
+  for (int i = 0; i < n.length; i++) {
+    rho += int.parse(n[i]);
+  }
+  return rho;
+}
+
+int radiceNumericaRicorsiva(String n, {int index = 0, int rho = 0}) {
+  if (index == (n.length)) return rho;
+  rho += int.parse(n[index]);
+  index++;
+  return radiceNumericaRicorsiva(n, index: index, rho: rho);
+}
